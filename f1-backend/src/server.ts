0bfd5e947ff375dba40adf2d649f90e372e6f259
Michela Federico <federico.michela@gmail.com>
2024-10-22 12:38:21 +0100

    [f1-backend] implemented two endpoints to retrieve races and competitions minimal infos for mvp

--- f1-backend/src/server.ts
+++ f1-backend/src/server.ts
@@ -1,18 +1,19 @@
 import express from 'express';
 import axios from 'axios';
+import {parseCompetition, parseRace} from "./parsers";
 
 const app = express();
 const port = 10000;
 
-// Route to retrieve the race winner
-app.get('/api/schedules', async (req:any, res:any) => {
+// Route to retrieve the races
+app.get('/api/races', async (req:any, res:any) => {
     let { year } = req.query;
 
     year = year || new Date().getFullYear();
 
     const options = {
         method: 'GET',
-        url: `https://f1-motorsport-data.p.rapidapi.com/schedule?year=${year}`,
+        url: `https://f1-motorsport-data.p.rapidapi.com/scoreboard?year=${year}`,
         headers: {
             'x-rapidapi-host': 'f1-motorsport-data.p.rapidapi.com',
             'x-rapidapi-key': '7455d68b55msh4ba37d94f2246d6p1e6b7cjsned4538fcbb3e',
@@ -21,17 +22,52 @@ app.get('/api/schedules', async (req:any, res:any) => {
 
     try {
         const response = await axios.request(options);
-        const schedules = response.data;
+        const scoreboard = response.data.scoreboard;
 
-        if (schedules) {
-            res.json({ schedules });
+        if (scoreboard) {
+            const parsedRaces = scoreboard.map(parseRace);
+
+            res.json({ races: parsedRaces });
         } else {
             res.status(404).json({ message: `Schedule not found for year ${year}` });
         }
     } catch (error) {
+        console.error(error)
         res.status(500).json({ message: 'Error fetching race data' });
     }
 });
+
+// Route to retrieve the race winner
+app.get('/api/races/:raceId/competitions', async (req:any, res:any) => {
+    const {raceId} = req.params;
+
+    const options = {
+        method: 'GET',
+        url: `https://f1-motorsport-data.p.rapidapi.com/race-report?eventId=${raceId}`,
+        headers: {
+            'x-rapidapi-host': 'f1-motorsport-data.p.rapidapi.com',
+            'x-rapidapi-key': '7455d68b55msh4ba37d94f2246d6p1e6b7cjsned4538fcbb3e',
+        },
+    };
+
+    try {
+        const response = await axios.request(options);
+        const competitions = response.data.report.positions;
+
+        if (competitions) {
+            const parsedCompetitions = competitions.map(parseCompetition);
+
+            res.json({ competitions: parsedCompetitions });
+        } else {
+            res.status(404).json({ message: `Competitions not found for race ${raceId}` });
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Error fetching race data' });
+    }
+});
+
+
 
 app.listen(port, () => {
     console.log(`Backend server running at http://localhost:${port}`);
